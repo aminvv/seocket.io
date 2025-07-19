32d0c8584a3a4d639b58113f44c7de70012e9909
aminvv <aminviper1378@gmail.com>
2025-07-19 13:29:17 +0330

    nameSpace in  socket.io

--- client.js
+++ client.js
@@ -1,11 +1,31 @@
 const socket = io("http://localhost:3000");
 
+socket.on("broadcast", data => {
+  console.log("📩 From broadcast server:", data);
+});
+
 socket.on("connect", () => {
-    console.log("✅ Connected to server");
+  console.log("✅ Connected to server");
+  socket.emit("clientMessage", "hello server");
+  socket.on("serverMessage", (data) => {
+    console.log("📩 From server:", data);
+  });
+});
 
-    socket.emit("clientMessage", "hello server");
+const socketStudent = io("http://localhost:3000/student");
+socketStudent.on("connect", () => {
+  console.log("✅ Connected to server student");
+  socketStudent.on("messageSocketStudent", (data) => {
+    console.log("📩 From student server:", data);
+  });
+  socketStudent.emit("clientMessage", "hello socketStudent server");
+});
 
-    socket.on("serverMessage", (data) => {
-        console.log("📩 From server:", data);
-    });
+const socketTeacher = io("http://localhost:3000/teacher");
+socketTeacher.on("connect", () => {
+  console.log("✅ Connected to server teacher");
+  socketTeacher.on("messageSocketTeacher", (data) => {
+    console.log("📩 From teacher server:", data);
+  });
+  socketTeacher.emit("clientMessage", "hello socketTeacher server");
 });
--- server.js
+++ server.js
@@ -2,24 +2,39 @@ const http = require("http");
 const express = require("express");
 const app = express();
 
-app.use(express.static("./")); // فایل‌های استاتیک از پوشه جاری
+app.use(express.static("./"));
 
 const server = http.createServer(app);
 const socketIO = require("socket.io");
 const io = socketIO(server, {
-    cors: {
-        origin: '*'
-    }
+  cors: {
+    origin: '*'
+  }
 });
 
 io.on("connection", (socket) => {
-    console.log("🟢 New client connected:", socket.id);
+  console.log("🟢 New client connected:", socket.id);
+  socket.on("clientMessage", data => {
+    console.log("📩 From client:", data);
+  });
+  socket.emit("serverMessage", "hello client");
+  io.emit("broadcast", "hello everyone");
+});
 
-    socket.on("clientMessage", data => {
-        console.log("📩 From client:", data);
-    });
+io.of("/student").on("connection", (socket) => {
+  console.log("🟢 New client connected student:", socket.id);
+  socket.on("clientMessage", data => {
+    console.log("📩 From client student:", data);
+  });
+  socket.emit("messageSocketStudent", "hello client student");
+});
 
-    socket.emit("serverMessage", "hello client");
+io.of("/teacher").on("connection", (socket) => {
+  console.log("🟢 New client connected teacher:", socket.id);
+  socket.on("clientMessage", data => {
+    console.log("📩 From client teacher:", data);
+  });
+  socket.emit("messageSocketTeacher", "hello teacher");
 });
 
 server.listen(3000, () => console.log("🚀 Server running on port 3000"));
